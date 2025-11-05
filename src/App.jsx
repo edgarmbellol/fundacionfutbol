8ea6fbc8950561b1d00e181d5a795e7ded4fdd00
Edgar Bellol <edgarmbellol@gmail.com>
2025-11-04 20:02:17 -0500

    Initial commit: CLUB VR Futsal website complete

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Schedule from './pages/Schedule';
+import Contact from './pages/Contact';
+import ScrollToTop from './components/ScrollToTop';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/nosotros" element={<About />} />
+            <Route path="/galeria" element={<Gallery />} />
+            <Route path="/horarios" element={<Schedule />} />
+            <Route path="/contacto" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
